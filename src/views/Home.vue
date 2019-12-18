94cd1a46621aa3e5d4dfe5057d52c7abbff20ae2
dev-1603 <53967786+dev-1603@users.noreply.github.com>
2019-12-18 06:59:49 +0530

    a

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <AddTodo @add-todo="addTodo" />
+    <Todos :todos="todos" @del-todo="deleteTodo" />
   </div>
 </template>
 
